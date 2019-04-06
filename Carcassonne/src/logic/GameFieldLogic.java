e7824d9f397ba8984badf83bbba0f175919c3868
andre-greubel <andre.greubel@uni-wuerzburg.de>
2019-04-06 23:44:01 +0200

    Fixed missing ! in TileLogic, Added stub GameFieldLogic

---
+++ Carcassonne/src/logic/GameFieldLogic.java
@@ -0,0 +1,15 @@
+package logic;
+
+import model.GameField;
+import model.Tile;
+import java.util.*;
+
+public class GameFieldLogic {
+
+
+    public static Map<Type, Collection<Tile>> getClosedAreas(GameField field, Position pos, Tile addedTile){
+        return null;
+    }
+
+
+}
--- Carcassonne/src/logic/TileLogic.java
+++ Carcassonne/src/logic/TileLogic.java
@@ -42,7 +42,7 @@ public class TileLogic {
         for (Direction dir : Direction.values()) {
             for (Type type : Type.values()) {
                 if (tile.isExtendable(dir, type)) {
-                    if (result.containsKey(dir)) {
+                    if (!result.containsKey(dir)) {
                         result.put(dir, new ArrayList<>());
                     }
                     result.get(dir).add(type);
