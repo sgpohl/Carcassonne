7f293a505c097abda8088095c1e2698b4c74810e
zora-pidde <zora.pidde@web.de>
2019-04-06 17:07:28 +0200

    changed package name in class Position set up creator for Position excluded former method from Tile

--- Carcassonne/src/model/Tile.java
+++ Carcassonne/src/model/Tile.java
@@ -2,12 +2,13 @@ package model;
 
 import logic.Direction;
 import logic.Type;
+import java.util.*;
 
 public interface Tile {
 	
 	public boolean isExpendable(Direction direction, Type type);
 	
-	public Map[Direction, List[Type]] getExpandableOptions();
+	
 	
 
 }
