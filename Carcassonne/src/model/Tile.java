ecdb298645e32a0c9496a119ca0056eb52ca7542
zora-pidde <zora.pidde@web.de>
2019-04-06 16:56:56 +0200

    found GameField start implementing Tile

--- Carcassonne/src/model/Tile.java
+++ Carcassonne/src/model/Tile.java
@@ -1,5 +1,13 @@
 package model;
 
+import logic.Direction;
+import logic.Type;
+
 public interface Tile {
+	
+	public boolean isExpendable(Direction direction, Type type);
+	
+	public Map[Direction, List[Type]] getExpandableOptions();
+	
 
 }
