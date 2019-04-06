3d71fe07aa53d3b6b4757ce5ff50b44be4a7f663
Steffen Pohl <steffen.pohl@gmx.de>
2019-04-06 16:47:45 +0200

    TileGraphic stub

---
+++ Carcassonne/src/view/TileGraphic.java
@@ -0,0 +1,14 @@
+package view;
+import java.awt.*;
+
+public class TileGraphic extends Canvas {
+	
+	public TileGraphic() {
+		super();
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+	}
+}
--- Carcassonne/src/view/UI.java
+++ Carcassonne/src/view/UI.java
@@ -1,5 +1,28 @@
 package view;
+import logic.*;
+
+import javax.swing.*;
 
 public class UI {
-
+	
+	JFrame frame;
+	
+	public UI() {
+		frame = new JFrame();
+		frame.setSize(500, 500);
+		frame.setVisible(true);
+	}
+	
+	
+	public void draw(Position pos, model.Tile tile) {
+		
+	}
+	
+	public void highlight(Position pos, boolean isEmpty) {
+		
+	}
+	
+	public static void main(String[] args) {
+		new UI();
+	}
 }
