962c2e1fa5026c75375dfc936774e39443206e4c
Steffen Pohl <steffen.pohl@gmx.de>
2019-04-07 17:14:09 +0200

    Added UI rotation callback to controller

--- Carcassonne/src/controller/Controller.java
+++ Carcassonne/src/controller/Controller.java
@@ -6,4 +6,7 @@ public class Controller {
 	public void UI_clickedOnTile(Position pos) {
 		System.out.println("clicked at "+pos.toString());
 	}
+	public void UI_rotateCurrentTile() {
+		System.out.println("do teh rotato thing");
+	}
 }
