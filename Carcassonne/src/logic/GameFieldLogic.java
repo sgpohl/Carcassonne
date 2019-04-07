9f77dee21167d5f29b595bee8f50e85e82a4035f
zora-pidde <zora.pidde@web.de>
2019-04-07 15:56:44 +0200

    Implemented helping function 	test for a given file & type & dircection if area is closed

--- Carcassonne/src/logic/GameFieldLogic.java
+++ Carcassonne/src/logic/GameFieldLogic.java
@@ -1,17 +1,27 @@
 package logic;
 
 import model.GameField;
+import java.util.*;
 import model.Tile;
+import logic.Direction;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Map;
+//import java.util.Collection;
+//import java.util.HashSet;
+//import java.util.Map;
 import java.util.stream.Collectors;
 
 public class GameFieldLogic {
 
-
+	// startAtPos: Position des neu angelegten Tiles
+	//Dummy
+	// Directions.SOUTH == dummy
     public static Map<Type, Collection<Tile>> getClosedAreas(GameField field, Position startAtPos) {
+    	Map<Type, Collection<Tile>> returnMap = new HashMap<>();
+    	GameFieldExtensionSearch toSearch = new GameFieldExtensionSearch();
+    	for(Type t: Type.values()) {
+    		toSearch.isConnected(field, startAtPos, t, Direction.SOUTH);
+    	}
+    	
         return null;
     }
 
@@ -20,6 +30,28 @@ public class GameFieldLogic {
 
 class GameFieldExtensionSearch {
     private Collection<Position> closed = new HashSet<Position>();
+    
+    public GameFieldExtensionSearch() {
+    }
+    
+    // Hilfsfunktion die Testet ob INNERHALB eines gegebenen Tiles ein bestimmter Type abgeschlossen ist
+    // (aus bestimmter Richtung gesehen)
+	public static boolean isSingleTileClosed(Tile t, Type ty, Direction startDirection) {
+		if (t.isExtendable(startDirection, ty)) { // has this direction the asked type?
+			boolean clockwiseEqualType = t.isExtendable(startDirection.rotateClockwise(), ty);
+			boolean counterClockwiseEqualType = t.isExtendable(startDirection.rotateCounterclockwise(), ty);
+			if (clockwiseEqualType || counterClockwiseEqualType) {
+				return false;
+			}
+			if (ty == Type.FOREST) {
+				return true;
+			}
+			if (t.isExtendable(startDirection.getOpposite(), ty)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
     // TODO: untested!
     public boolean isConnected(GameField gameField, Position pos, Type type, Direction enteredFromDirection) {
