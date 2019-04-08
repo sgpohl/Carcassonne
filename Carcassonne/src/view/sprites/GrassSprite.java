41678110b3b8b4d6125c21ef22c4a09e49bc25a6
Steffen Pohl <steffen.pohl@gmx.de>
2019-04-08 16:24:21 +0200

    UI: added wheat sprites

---
+++ Carcassonne/src/view/sprites/GrassSprite.java
@@ -0,0 +1,75 @@
+package view.sprites;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class GrassSprite extends Sprite {
+	private static final int strawCount = 10;
+	private static final float strawDensity = 0.5f;
+	
+	
+	private class StrawSprite extends Sprite {
+		private float angle;
+		public StrawSprite(Point pos, float angle) {
+			super(pos);
+			this.angle = angle;
+		}
+		
+		@Override
+		public void bakeInto(Graphics2D g) {
+			int height = 10;
+			
+			var oldTransform = g.getTransform();
+			
+			g.translate(position.x, position.y);
+			g.rotate(angle);
+			
+			//g.setColor(new Color(160,160,70));
+			g.setColor(new Color(110,110,40));
+			g.setStroke(new BasicStroke(3));
+			g.drawLine(0,0,0,-height);
+			g.fillOval(0,0-height-5, 3, 10);
+			
+			g.setColor(new Color(180,180,80));
+			g.setStroke(new BasicStroke(1));
+			g.drawLine(0,0,0,-height);
+			g.fillOval(0,0-height-4, 2, 8);
+			
+			g.setTransform(oldTransform);
+		}
+		
+	};
+	
+	private List<StrawSprite> straws;
+	
+	public GrassSprite(Point pos) {
+		super(pos);
+		straws = new ArrayList<StrawSprite>();
+		
+		for(int i = 0; i < strawCount; ++i) {
+			int x = (int)(Math.random()*strawCount/strawDensity);
+			int y = (int)(Math.random()*strawCount/strawDensity/2);
+			float angle = (float) (Math.random()-0.5f);
+			straws.add(new StrawSprite(new Point(x,y), angle));
+		}
+		Collections.sort(straws, Comparator.comparing(straw -> straw.getPosition().y));
+	}
+	
+	@Override
+	public void bakeInto(Graphics2D g) {
+		var oldTransform = g.getTransform();
+		g.translate(position.x, position.y);
+		
+		for(var straw : straws)
+			straw.bakeInto(g);
+		
+		g.setTransform(oldTransform);
+	}
+
+}
