645efe8ce85876e092acd0d0b0858164013e8292
Steffen Pohl <steffen.pohl@gmx.de>
2019-04-09 01:43:45 +0200

    UI: Meeple shading

--- Carcassonne/src/view/sprites/MeepMeeple.java
+++ Carcassonne/src/view/sprites/MeepMeeple.java
@@ -1,5 +1,6 @@
 package view.sprites;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Point;
@@ -11,6 +12,26 @@ import java.util.Collection;
 
 public class MeepMeeple extends Sprite {
 	Collection<Shape> body;
+	private static final Color[] playerColors = {	new Color(255,0,0), 
+													new Color(255,255,0),
+													new Color(0,255,0),
+													new Color(0,0,255),
+													new Color(255,0,255),
+													new Color(0,255,255),
+													new Color(255,255,255),
+													new Color(0,0,0),
+												};
+
+	private static final Color[] playerShades = {	playerColors[0].darker(), 
+													playerColors[1].darker(),
+													playerColors[2].darker(),
+													playerColors[3].darker(),
+													playerColors[4].darker(),
+													playerColors[5].darker(),
+													playerColors[6].darker(),
+													new Color(60,60,60)
+												};
+	private static int colorUsed = 0;
 	
 	private int scale(int value) {
 		return (int)(value*1.);
@@ -28,9 +49,16 @@ public class MeepMeeple extends Sprite {
 
 	@Override
 	public void bakeInto(Graphics2D g) {
-		g.setColor(Color.YELLOW);
+		g.setColor(playerShades[colorUsed]);
+		g.setStroke(new BasicStroke(4));
+		for(var s : body)
+			g.draw(s);
+		
+		g.setColor(playerColors[colorUsed]);
 		for(var s : body)
 			g.fill(s);
+		
+		colorUsed = (colorUsed+1)%playerColors.length;
 	}
 	
 
